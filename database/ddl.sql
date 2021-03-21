f45d689a858784df87561f2b33b3f0bc2a963afb
HerbLuo <cloudself.cn@gmail.com>
2021-03-21 21:59:05 +0800

    ddl

--- database/ddl.sql
+++ database/ddl.sql
@@ -10,12 +10,13 @@ CREATE TABLE version (
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    KEY with_type_with_uid_business (`type`, `uid`, `business`) /*注意这里为了更好的使用索引，搜索全局业务时，加上uid is null更好*/
 ) COLLATE = utf8mb4_unicode_ci COMMENT '版本信息';
 
 CREATE TABLE version_command (
     id          bigint unsigned                    AUTO_INCREMENT PRIMARY KEY,
-    uid         bigint unsigned                    NULL COMMENT '当数据私有时可选',
+    uid         bigint unsigned                    NULL     COMMENT '当数据私有时可选',
     business    varchar(32)                        NOT NULL COMMENT '业务',
     version     int unsigned                       NOT NULL COMMENT '版本',
     command     varchar(255)                       NOT NULL COMMENT '同步用的指令',
@@ -24,7 +25,8 @@ CREATE TABLE version_command (
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    KEY with_type_with_uid_business (`business`, `uid`, `version`)
 ) COLLATE = utf8mb4_unicode_ci COMMENT '版本同步指令';
 
 /* 公共数据 */
@@ -36,8 +38,7 @@ CREATE TABLE simple_word ( /*Redis: Set<word>, FrontEnd: Array<word>*/
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    constraint word unique (word)
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '简单词汇';
 
 CREATE TABLE word ( /*Redis: ZSet<first word, word, score>, FrontEnd: Record<word, score>*/
@@ -62,8 +63,7 @@ CREATE TABLE professional_word (/*Redis: Hash<category, word, score>, FrontEnd: 
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    CONSTRAINT category UNIQUE (category_id, word)
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '专业词汇';
 
 CREATE TABLE exchanges (/*Redis: Hash<first word, word, origin>, FrontEnd: Record<word, score>*/
@@ -106,14 +106,13 @@ CREATE TABLE verify (
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY `tmp_token` (`tmp_token`)
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '审批表';
 
 /* 用户登陆信息 */
 
 CREATE TABLE auth_pwd (
-    uid         bigint unsigned                    NOT NULL COMMENT 'uid' primary key,
+    uid         bigint unsigned                    NOT NULL PRIMARY KEY,
     username    varchar(50)                        NOT NULL COMMENT '登录用的用户名',
     password    char(64)                           NOT NULL COMMENT '密码',
     disabled    tinyint(1)                         NOT NULL COMMENT '是否冻结',
@@ -123,11 +122,11 @@ CREATE TABLE auth_pwd (
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
     update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY `username` (`username`)
+    UNIQUE KEY username (`username`)
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户名密码信息';
 
 CREATE TABLE auth_tmp_token (
-    uid         bigint unsigned                    NOT NULL COMMENT 'uid' primary key,
+    uid         bigint unsigned                    NOT NULL PRIMARY KEY,
     tmp_token   char(36)                           NOT NULL COMMENT '临时账号可以使用临时token登录',
     disabled    tinyint(1)                         NOT NULL COMMENT '是否冻结',
 
@@ -136,7 +135,7 @@ CREATE TABLE auth_tmp_token (
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
     update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY `tmp_token` (`tmp_token`)
+    UNIQUE KEY tmp_token (`tmp_token`)
 ) COLLATE = utf8mb4_unicode_ci COMMENT '临时账号信息';
 
 /* 用户私有信息 */
@@ -152,7 +151,7 @@ CREATE TABLE user_pri_word ( /*FrontEnd: Record<word, familiar>*/
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
     update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY `uid_word` (`uid`,`word`)
+    UNIQUE KEY uid_word (`uid`,`word`)
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户私有的词汇';
 
 CREATE TABLE user_category_score ( /*FrontEnd: Record<category, score>*/
@@ -165,8 +164,7 @@ CREATE TABLE user_category_score ( /*FrontEnd: Record<category, score>*/
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    UNIQUE KEY `uid_word` (`uid`,`word`)
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户词汇分数';
 
 CREATE TABLE user_pri_setting (
@@ -175,14 +173,14 @@ CREATE TABLE user_pri_setting (
     kee         varchar(20)                        NOT NULL COMMENT '设置的键',
     val         varchar(50)                        NULL     COMMENT '设置的值，如果为null表从全局设置继承',
     site        varchar(100) default 'global'      NOT NULL COMMENT '生效的站点',
-    order       double default 0                   NULL     COMMENT '生效顺序',
+    `order`     double default 0                   NULL     COMMENT '生效顺序',
 
     deleted     tinyint(1) default 0               NOT NULL COMMENT '是否删除',
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
     update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    constraint uid_kee unique (uid, kee)
+    constraint uid_kee unique (uid, site, kee)
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户私有设置';
 
 CREATE TABLE user_pri (
@@ -193,7 +191,7 @@ CREATE TABLE user_pri (
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户私有数据';
 
 CREATE TABLE user_pub (
@@ -204,5 +202,5 @@ CREATE TABLE user_pub (
     create_by   varchar(55)                        NOT NULL COMMENT '创建人',
     create_time datetime default current_timestamp NOT NULL COMMENT '创建日期',
     update_by   varchar(55)                        NULL     COMMENT '更新人',
-    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    update_time datetime                           NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COLLATE = utf8mb4_unicode_ci COMMENT '用户公有数据';
